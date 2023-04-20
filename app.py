6895301cea675274741958557cf1d6540f24af55
Gonzalo Pincheira A <g.pincheira.a@gmail.com>
2023-04-20 02:10:13 -0400

    chore: update pages

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html', user = request.args.get('user'))
+    return render_template('welcome.html', user = request.args.get('username'))
